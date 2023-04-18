b5fb4508825614df5896e90619ec16c5d0516e1b
Soham-123-code <sohamagarkar@gmail.com>
2023-04-18 12:10:57 +0400

    code repeats string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(wordString, range) {
+    if(range < 0){
+        return 'ERROR'
+    }
+    let word = "";
+    for(let i = 0; i<range; i++) {
+        word += wordString
+    }
+    return word
 };
 
 // Do not edit below this line
