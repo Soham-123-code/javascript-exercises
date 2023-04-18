b78e3c7f2a982dc6f97bbbed031c6061df08fe63
Soham-123-code <sohamagarkar@gmail.com>
2023-04-18 12:29:21 +0400

    write deletion code

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, item) {
+    for(let i=0; i<arr.length(); i++) {
+        if(arr[i] === item){
+            delete arr[i];
+        }
+    }
 };
 
 // Do not edit below this line
