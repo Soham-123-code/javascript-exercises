55532cdb6359365d6a94207914b1a187170837fd
Soham-123-code <sohamagarkar@gmail.com>
2023-04-20 07:36:52 +0400

    sommented some old code and added new code with new logic

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,13 @@
-const removeFromArray = function(arr, item) {
-    for(let i=0; i<arr.length; i++) {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter(item => !includes(item))
+    /*for(let i=0; i<arr.length; i++) {
         if(arr[i] === item){
             const x = arr.splice(i,1);
         }
     }
     for(let j=0; j<arr.length; j++) {
         return arr;
-    }
+    }*/
 };
 
 // Do not edit below this line
