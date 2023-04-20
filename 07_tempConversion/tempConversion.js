5edf868f4231f91041122486e9d20eb83f5ce51d
Soham-123-code <sohamagarkar@gmail.com>
2023-04-21 01:35:01 +0400

    initial code write

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  var c = (f-32)*0.55;
+  return c.toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  var f = (c/0.55)+32;
+  return f.toFixed(1);
 };
 
 // Do not edit below this line
