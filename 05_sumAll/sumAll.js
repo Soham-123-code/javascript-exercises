123d0ddaec64315200b9b2f55a03e7ac0f4d5981
Soham-123-code <sohamagarkar@gmail.com>
2023-04-20 10:52:33 +0400

    inital write

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(base, range) {
+    const sum=0;
+    for(i=range; i!=base; i--) {
+        sum+=range;
+    }
+    return sum;
 };
 
 // Do not edit below this line
