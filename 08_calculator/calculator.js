1f9cb2a3904d4bfbc45bccadc95643bb9114f3e8
Soham-123-code <sohamagarkar@gmail.com>
2023-04-23 14:41:51 +0400

    inital code write

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum=0
+  for(let i=0; i<arr.length; i++){
+    sum+=arr[i]
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(a,b) {
+  return a*b
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let answer = 1;
+  if (n == 0 || n == 1){
+    return answer;
+  }
+  else if(n > 1){
+    for(var i = n; i >= 1; i--){
+      answer = answer * i;
+    }
+    return answer;
+  }
+  else{
+    return "number has to be positive."
+  } 
 };
 
 // Do not edit below this line
