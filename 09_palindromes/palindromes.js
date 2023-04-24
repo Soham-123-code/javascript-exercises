76dc0280fa5933e573e07b733904fa3c1f5c19b3
Soham-123-code <sohamagarkar@gmail.com>
2023-04-24 19:37:59 +0400

    minor change

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -2,7 +2,7 @@ const palindromes = function (pali) {
     let a = /\s+/g;
     let b = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/
     let solution = pali.replace(a, '').replace(b, ' ')
-    let splitter = solution.split()
+    let splitter = solution.split("")
     let reverser = splitter.reverse()
     let joiner = reverser.join()
     if(solution===joiner){
