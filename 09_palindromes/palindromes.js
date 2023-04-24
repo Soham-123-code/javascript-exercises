9c4f06a153271d8c9972d0bc1deca6dbb2fd6d52
Soham-123-code <sohamagarkar@gmail.com>
2023-04-24 19:49:58 +0400

    make logic changes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,21 +1,8 @@
 const palindromes = function (pali) {
     let a = /\s+/g;
     let b = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/
-    //string with removed spaces and punctuation
-    let solution = pali.replace(a, '').replace(b, ' ')
-
-    //string reversal
-    let splitter = solution.split("")
-    let reverser = splitter.reverse()
-    let joiner = reverser.join("")
-
-    //palindrome checker
-    if(solution===joiner){
-        return true
-    }
-    else {
-        return false
-    }
+    const sol = pali.toLowerCase().replace(a, "").replace(b, "")
+    return pali.split("").reverse().join("")==sol
 };
 
 // Do not edit below this line
