add88aebaaf1d7abd8ce24c866d0f787cb068377
Soham-123-code <sohamagarkar@gmail.com>
2023-04-24 19:26:35 +0400

    initial code write

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (pali) {
+    let a = /\s+/g;
+    let b = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/
+    let solution = pali.replace(a, '').replace(b, ' ')
+    let splitter = solution.split()
+    let reverser = splitter.reverse()
+    let joiner = reverser.join()
+    if(solution===joiner){
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
