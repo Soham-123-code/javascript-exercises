621b26017b9e6fecc1a0c461855032a7ce22ec06
Soham-123-code <sohamagarkar@gmail.com>
2023-04-24 19:41:46 +0400

    code overhaul

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,10 +1,15 @@
 const palindromes = function (pali) {
     let a = /\s+/g;
     let b = /[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/
+    //string with removed spaces and punctuation
     let solution = pali.replace(a, '').replace(b, ' ')
+
+    //string reversal
     let splitter = solution.split("")
     let reverser = splitter.reverse()
-    let joiner = reverser.join()
+    let joiner = reverser.join("")
+
+    //palindrome checker
     if(solution===joiner){
         return true
     }
